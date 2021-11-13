1870260719731280f37b60b8cdcd3fcb4f62b191
Ahmad Anan Rafsanjani <appdev.ananrafs1@gmail.com>
2021-11-13 12:59:17 +0700

    change fb to email

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 let data = {
-    'fb': 'https://www.facebook.com/arafsanjani1',
+    'em': 'mailto:hi@ananrafs.com',
     'tw': 'https://www.twitter.com/ananrafs',
     'li': 'https://www.linkedin.com/in/ahmad-anan-rafsanjani-3b9bb2101/',
     'tm': 'https://ananrafs.tumblr.com/'
